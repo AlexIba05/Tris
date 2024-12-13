9c8f1f65c7e544ec35319579674c0ffbd0e9a93f
Alexander <alexanderiba005@gmail.com>
2024-12-13 11:19:16 +0100

    Passo 4

--- src/App.java
+++ src/App.java
@@ -1,8 +1,10 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        Random r = new Random();
 
         char[][] campo = {
                 { '-', '-', '-' },
@@ -14,50 +16,66 @@ public class App {
         System.out.println("Tic Tac Toe");
         System.out.println("-------------------------");
 
-        char Giocatore1= 'X';
-        char Giocatore2= 'O';
-        boolean turno= true;
+        char Giocatore1 = 'X';
+        char Giocatore2 = 'O';
+        boolean turno = true;
 
-        for ( int mosse = 0; mosse < 9; mosse++) {
+        for (int mosse = 0; mosse < 9; mosse++) {
 
             char turnoCorrente;
 
             if (turno) {
-                turnoCorrente= Giocatore1;
+                turnoCorrente = Giocatore1;
+            } else {
+                turnoCorrente = Giocatore2;
             }
-            else {
-                turnoCorrente= Giocatore2;
-            }
 
-            System.out.println("\nMossa numero " + (mosse + 1));
+            if (turno) {
 
-            boolean punto = false;
-            int riga = 0, colonna = 0;
+                System.out.println("Mossa numero " + (mosse + 1) + " - Giocatore Utente (" + turnoCorrente + ")");
 
-            while (!punto) {
-                System.out.print("Inserisci la riga (1-3): ");
-                riga = sc.nextInt();
+                boolean punto = false;
+                int riga = 0, colonna = 0;
 
-                System.out.print("Inserisci la colonna (1-3): ");
-                colonna = sc.nextInt();
+                while (!punto) {
+                    System.out.print("Inserisci la riga (1-3): ");
+                    riga = sc.nextInt();
 
-                if (riga < 1 || riga > 3 || colonna < 1 || colonna > 3) {
-                    System.out.println("Coordinata non valida");
-                    continue;
+                    System.out.print("Inserisci la colonna (1-3): ");
+                    colonna = sc.nextInt();
+
+                    if (riga < 1 || riga > 3 || colonna < 1 || colonna > 3) {
+                        System.out.println("Coordinata non valida");
+                        continue;
+                    }
+
+                    if (campo[riga - 1][colonna - 1] != '-') {
+                        System.out.println("Questa posizione è già occupata");
+                        continue;
+                    }
+
+                    punto = true;
+
                 }
 
-                if (campo[riga - 1][colonna - 1] != '-') {
-                    System.out.println("Questa posizione è già occupata");
-                    continue;
+                campo[riga - 1][colonna - 1] = turnoCorrente;
+            } else {
+
+                System.out.println("Mossa numero " + (mosse + 1) + " - Giocatore Computer (" + turnoCorrente + ")");
+                boolean sceltaPc = false;
+
+                while (!sceltaPc) {
+                    int rigaPc = r.nextInt(3);
+                    int colonnaPc = r.nextInt(3);
+
+                    if (campo[rigaPc][colonnaPc] == '-') {
+                        campo[rigaPc][colonnaPc] = turnoCorrente;
+                        sceltaPc = true;
+                    }
+
                 }
 
-                punto = true;
-
             }
-
-
-            campo[riga - 1][colonna - 1] = turnoCorrente;
-
             // stampa campo
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
@@ -66,12 +84,12 @@ public class App {
                 System.out.println(" ");
             }
 
-            turno= !turno;
-
+            turno = !turno;
         }
 
         System.out.println("Partita terminata");
-        sc.close();
 
+        sc.close();
     }
+
 }
